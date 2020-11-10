978b20996e0f59b6c926bf77236198d8d6d0eb09
Wangmin <691207572@qq.com>
2020-11-10 16:06:13 +0800

    添加aes加密

--- tests/AES.php
+++ tests/AES.php
@@ -16,8 +16,10 @@ class AES extends TestCase
     public function testEncrypt()
     {
         $iv = str_repeat("\0", openssl_cipher_iv_length('aes-128-cbc'));
-        $ase = new \stlswm\PHPEncrypt\AES(base64_decode('BvJRy6KPOzWhZOgS0/dkPA=='), 'aes-128-cbc', $iv,
+        $ase = new \stlswm\PHPEncrypt\AES(base64_decode('MTExMTExMTExMTExMTExMQ=='), 'aes-128-cbc', $iv,
             OPENSSL_RAW_DATA);
-        $this->assertSame('zpS/oYW63YrN197BEcTVqw==', base64_encode($ase->encrypt('123')));
+        $e = base64_encode($ase->encrypt('123'));
+        $d = $ase->decrypt(base64_decode($e));
+        $this->assertSame('123',$d);
     }
 }
